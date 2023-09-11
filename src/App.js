bbb18a9525aa334fbd3f9724f231bf75c8efd968
Alejandro Villamayor <avillamayor720@gmail.com>
2023-09-11 17:53:17 -0300

    Primer push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LotteryResultNumberEditor from './components/LotteryResultNumberEditor/LotteryResultNumberEditor';
+
+function App() {
+  return (
+    <div >
+      <LotteryResultNumberEditor/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LotteryResultNumberEditor/LotteryResultNumberEditor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LotteryResultNumberEditor() {
+  return (
+    <div>
+      hola
+    </div>
+  )
+}
