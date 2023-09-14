a2ff5e39679397dba28aa4ad54860da97a241e65
Alejandro Villamayor <avillamayor720@gmail.com>
2023-09-14 13:07:36 -0300

    Agregados ejemplos de métodos de pago. Esperando información adicional del cliente.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import LotteryResultNumberShow from "./components/LotteryResultNumberShow/LotteryResultNumberShow";
+import PaymentMethods from "./components/PaymentMethods/PaymentMethods";
 
 function App() {
   return (
     <div>
       <LotteryResultNumberShow />
+      <Routes>
+        <Route path="/payment" element={<PaymentMethods />} />
+      </Routes>
     </div>
   );
 }
