5168de4bbf47cd78fc9526f4eecc85a53c1cff25
TamaraZalavsky82 <tamaraanabellazaslavsky@gmail.com>
2023-09-17 21:24:18 -0300

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+import {CiFacebook, CiInstagram} from 'react-icons/ci'
+
+function Footer() {
+  return (
+    <div >
+      <footer className='footer'>
+soy footer
+      </footer>
+      <div className='redes'>
+<CiFacebook/>
+<CiInstagram/>
+</div>
+    </div>
+  )
+}
+
+export default Footer
